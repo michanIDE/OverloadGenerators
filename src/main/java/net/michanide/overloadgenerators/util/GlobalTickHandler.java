54a4a160437bdb14045d80a8144514d0c89bd673
michanIDE <init.complete@gmail.com>
2025-05-08 00:07:04 +0900

    Modified GlobalTickHandler

--- src/main/java/net/michanide/overloadgenerators/util/GlobalTickHandler.java
+++ src/main/java/net/michanide/overloadgenerators/util/GlobalTickHandler.java
@@ -1,5 +1,10 @@
 package net.michanide.overloadgenerators.util;
 
+import java.util.List;
+
+import org.checkerframework.checker.units.qual.s;
+import org.lwjgl.system.CallbackI.S;
+
 import net.michanide.overloadgenerators.OverloadGenerators;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.event.TickEvent;
@@ -7,33 +12,56 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
+import oshi.hardware.PowerSource;
 
 @Mod.EventBusSubscriber(modid = OverloadGenerators.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class GlobalTickHandler {
     private static double cachedCPUUsage = 0;
     private static long cachedTickTime = 0;
+    // private static double cachedPowerUsage = 0;
     private static long lastTick = -1;
-    private static int CPUUsageCounter = 20;
+    private static int systemInfoCounter = 20;
     private static int tickTimeCounter = 0;
     private static int tickTimeCounterMax = 100;
     private static long[] prevTicks = null;
-    private static boolean isCPUUsageActive = true;
+    private static boolean isSystemInfoActive = true;
     private static boolean isTickTimeActive = true;
 
     @SubscribeEvent
     public static void onWorldTick(TickEvent.WorldTickEvent event) {
         long currentTick = event.world.getGameTime();
         if (currentTick != lastTick && event.phase == TickEvent.Phase.END && !event.world.isClientSide) {
-            if (isCPUUsageActive) {
-                lastTick = currentTick;
-                if(CPUUsageCounter > 19){
-                    CPUUsageCounter = 0;
-                    cachedCPUUsage = getCPUUsage();
-                    isCPUUsageActive = false;
-                } else {
-                    CPUUsageCounter++;
+
+            lastTick = currentTick;
+
+            // SystemInfo 
+            if (systemInfoCounter > 19) {
+                systemInfoCounter = 0;
+
+                if (isSystemInfoActive) {
+                    try {
+                        SystemInfo systemInfo = new SystemInfo();
+                        
+                        // CPU Usage
+                        cachedCPUUsage = getCPUUsage(systemInfo);
+
+                        // Power Usage
+                        // cachedPowerUsage = getPowerUsage(systemInfo);
+                        // System.out.println(cachedPowerUsage);
+
+                        isSystemInfoActive = false;
+
+                    } catch (Exception e) {
+                        cachedCPUUsage = 0.0;
+                        // cachedPowerUsage = 0.0;
+                        isSystemInfoActive = false;
+                    }
                 }
+            } else {
+                systemInfoCounter++;
             }
+
+            // Tick Time
             if (isTickTimeActive) {
                 MinecraftServer server = event.world.getServer();
                 if (server != null) {
@@ -49,15 +77,13 @@ public class GlobalTickHandler {
                 tickTimeCounter = 0;
             }
         }
-        
     }
 
-    private static double getCPUUsage() {
+    private static double getCPUUsage(SystemInfo systemInfo) {
         
         double cpuUsage;
 
         try{
-            SystemInfo systemInfo = new SystemInfo();
             CentralProcessor processor = systemInfo.getHardware().getProcessor();
             if(prevTicks != null){
                 cpuUsage = processor.getSystemCpuLoadBetweenTicks(prevTicks);
@@ -71,8 +97,24 @@ public class GlobalTickHandler {
         return cpuUsage;
     }
 
+    // private static double getPowerUsage(SystemInfo systemInfo) {
+    //     try{
+    //         List<PowerSource> powerSources = systemInfo.getHardware().getPowerSources(); 
+    //         double powerUsage = 0.0;
+    //         for(PowerSource powerSource : powerSources){
+    //             powerUsage += powerSource.getPowerUsageRate();
+    //             System.out.print(powerSource.getName());
+    //             System.out.println(" Added");
+    //         }
+    //         return powerUsage;
+    //     } catch (Exception e){
+    //         System.out.println("cannnot get power usage");
+    //         return 0.0;
+    //     }
+    // }
+
     public static double getCachedCPUUsage() {
-        isCPUUsageActive = true;
+        isSystemInfoActive = true;
         return cachedCPUUsage;
     }
 
